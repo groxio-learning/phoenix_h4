4aba2cd6d9fd8d6f32c3a3ac1fb63e82045f0c6b
rharfordhacks <rory.harford@hiive.com>
2025-07-22 15:40:25 -0700

    add game and scoring system

--- breaker/lib/game.ex
+++ breaker/lib/game.ex
@@ -1,10 +1,12 @@
-defmodule Breaker.Game do 
-    defstruct answer: [1, 2, 3, 4], 
+defmodule Breaker.Game do
+    alias Breaker.Score
+
+    defstruct answer: [1, 2, 3, 4],
               guesses: [],
               scores: []
-    
+
     def new() do
-        %__MODULE__{} 
+        %__MODULE__{}
         |> scramble()
     end
 
@@ -23,13 +25,30 @@ defmodule Breaker.Game do
 
     def make_guess(game, guess) do
         %{game | guesses: [guess | game.guesses]}
+        |> score()
+        |> show()
     end
 
-    def score(game) do
-
+    def score(%{answer: answer, guesses: [guess | _guesses], scores: scores } = game) do
+        score = Score.new(answer, guess)
+        %{game | scores: [score | scores]}
     end
 
-    def show(game) do
+  def show(%{scores: [%{red: red, white: white} | _], guesses: guesses} = game) do
+    max_allowed_guesses = 2
+    number_of_guesses = Enum.count(guesses)
 
+    cond do
+        red == 4 ->
+        "<p>You win!</p>"
+
+        number_of_guesses >= max_allowed_guesses ->
+        "<p>Game over!</p>"
+
+        true ->
+            IO.puts("<p>reds: #{red} whites: #{white}</p>")
+            game
     end
+
+  end
 end
