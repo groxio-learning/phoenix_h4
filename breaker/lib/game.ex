465b0ce76e168c5b594829386af2a1e4cf5a9498
Li Ju <julia@hiive.com>
2025-07-23 09:50:59 -0700

    update the game show and score show

--- breaker/lib/game.ex
+++ breaker/lib/game.ex
@@ -1,53 +1,77 @@
 defmodule Breaker.Game do
-    alias Breaker.Score
+  alias Breaker.Score
 
-    defstruct answer: [1, 2, 3, 4],
-              guesses: [],
-              scores: []
-
-    def new() do
-        %__MODULE__{}
-        |> scramble()
-    end
-
-    def new(secret) do
-        %__MODULE__{
-            answer: secret,
+  defstruct answer: [1, 2, 3, 4],
             guesses: [],
             scores: []
-        }
+
+  @attempts 8
+
+  def new() do
+    %__MODULE__{}
+    |> scramble()
+  end
+
+  def new(secret) do
+    %__MODULE__{
+      answer: secret,
+      guesses: [],
+      scores: []
+    }
+  end
+
+  def scramble(game) do
+    secret = Enum.take_random(1..6, 4)
+    %{game | answer: secret}
+  end
+
+  def make_guess(game, guess) do
+    %{game | guesses: [guess | game.guesses]}
+    |> score()
+  end
+
+  def score(%{answer: answer, guesses: [guess | _guesses], scores: scores} = game) do
+    score = Score.new(answer, guess)
+    %{game | scores: [score | scores]}
+  end
+
+  def show(%{scores: scores, guesses: guesses} = game) do
+    # 1, 2, 3, 4 RRWW
+
+    board =
+      Enum.zip(guesses, scores)
+      |> Enum.map(&print_row/1)
+      |> Enum.join("\n")
+
+    answer = "? ? ? ?"
+
+    status = status(game)
+
+    """
+    #{answer}
+
+    #{board}
+
+    #{status}
+    """
+  end
+
+  defp print_row({guess, score}) do
+    guess_str = Enum.join(guess, ~S{ })
+    score_str = Score.show(score)
+
+    "| #{guess_str} | #{score_str}"
+  end
+
+  defp status(%{scores: [%{red: 4} | _scores]}) do
+    "You won!"
+  end
+
+  defp status(game) do
+    if length(game.guesses) >= @attempts do
+      "You lost"
+    else
+      "Make a guess"
     end
-
-    def scramble(game) do
-        secret = Enum.take_random(1..6, 4)
-        %{game | answer: secret}
-    end
-
-    def make_guess(game, guess) do
-        %{game | guesses: [guess | game.guesses]}
-        |> score()
-    end
-
-    def score(%{answer: answer, guesses: [guess | _guesses], scores: scores } = game) do
-        score = Score.new(answer, guess)
-        %{game | scores: [score | scores]}
-    end
-
-  def show(%{scores: [%{red: red, white: white} | _], guesses: guesses} = game) do
-    max_allowed_guesses = 2
-    number_of_guesses = Enum.count(guesses)
-
-    cond do
-        red == 4 ->
-        "<p>You win!</p>"
-
-        number_of_guesses >= max_allowed_guesses ->
-        "<p>Game over!</p>"
-
-        true ->
-            IO.puts("<p>reds: #{red} whites: #{white}</p>")
-            game
-    end
-
   end
 end
--- breaker/lib/score.ex
+++ breaker/lib/score.ex
@@ -1,22 +1,26 @@
 defmodule Breaker.Score do
-  defstruct  red: 0,
-    white: 0
+  defstruct red: 0,
+            white: 0
 
   def new(answer, guess) do
     red =
       answer
       |> Enum.zip(guess)
-      |> Enum.count(fn {a, g} -> a == g  end)
+      |> Enum.count(fn {a, g} -> a == g end)
 
     total = length(answer)
 
     missing = length(guess -- answer)
 
-    white = (total - red) - missing
+    white = total - red - missing
     %__MODULE__{red: red, white: white}
   end
 
-  def show(%{red: red , white: white}) do
+  def show(%{red: red, white: white}) do
+    # RRWW
+    converted_red = String.duplicate(~S{R}, red)
+    converted_white = String.duplicate(~S{W}, white)
 
+    "#{converted_red}#{converted_white}"
   end
 end
