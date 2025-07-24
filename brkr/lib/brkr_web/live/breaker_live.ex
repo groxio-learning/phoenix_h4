aeac575aff8c729123e7b0b6dda69ce8f644a7f8
Li Ju <julia@hiive.com>
2025-07-24 08:37:30 -0700

    reformat BreakerLive

--- brkr/lib/brkr_web/live/breaker_live.ex
+++ brkr/lib/brkr_web/live/breaker_live.ex
@@ -5,29 +5,49 @@ defmodule BrkrWeb.BreakerLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    socket = assign(socket, move: Move.new())
     game = Game.new()
-    socket = assign(socket, game: game)
-    socket = assign(socket, show: Game.show(game))
-    {:ok, socket}
+
+    {
+      :ok,
+      socket
+      |> assign(move: Move.new())
+      |> assign(game: game)
+      |> assign(show: Game.show(game))
+    }
   end
 
   @impl true
   def handle_event("add-move", %{"element" => element}, socket) do
-    move = socket.assigns.move |> Move.add(String.to_integer(element))
+    move =
+      socket.assigns.move
+      |> Move.add(String.to_integer(element))
+
     {:noreply, assign(socket, move: move)}
   end
 
   def handle_event("remove-move", _, socket) do
-    move = socket.assigns.move |> Move.remove()
+    move =
+      socket.assigns.move
+      |> Move.remove()
+
     {:noreply, assign(socket, move: move)}
   end
 
   def handle_event("submit", _, socket) do
+    {:noreply, submit(socket)}
+  end
+
+  defp submit(socket) do
     move = socket.assigns.move
-    game = socket.assigns.game |> Game.make_guess(move)
-    socket = assign(socket, game: game)
-    socket = assign(socket, show: Game.show(game))
-    {:noreply, assign(socket, move: Move.new())}
+
+    game =
+      socket.assigns.game
+      |> Game.make_guess(move)
+
+    socket =
+      socket
+      |> assign(game: game)
+      |> assign(show: Game.show(game))
+      |> assign(move: Move.new())
   end
 end
